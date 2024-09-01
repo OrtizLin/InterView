6974b095b5181d5c7006eb73baa5aaf2a159552b
Otis's Macbook Pro <fmradioo123@yahoo.com>
2024-09-02 00:38:22 +0800

    Initial Commit

---
+++ Interview/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Interview
+//
+//  Created by Otis on 2024/9/2.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
