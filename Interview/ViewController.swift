67afc39eab3198e0765fbe078c41c159e33d6cf5
Otis's Macbook Pro <fmradioo123@yahoo.com>
2024-09-06 00:47:34 +0800

    GCD cases

--- Interview/ViewController.swift
+++ Interview/ViewController.swift
@@ -12,8 +12,38 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        customQueue()
     }
 
-
+    private func mainQueue() { // Serial
+        print("task1")
+        DispatchQueue.main.async {
+            print("task2")
+        }
+        print("task3")
+    }
+    
+    private func globalQueue() { // Concurrent
+        print("task1")
+        DispatchQueue.global().sync {
+            print("task2")
+        }
+        print("task3")
+    }
+    
+    private func customQueue() { // Default Serial
+        let customQueue = DispatchQueue(label: "com.otis.testGCD")
+        print("task1")
+        DispatchQueue.global().async {
+            print("task2")
+            customQueue.sync {
+                print("task3")
+            }
+            print("task4")
+        }
+        print("task5")
+    }
+    
+    
 }
 
